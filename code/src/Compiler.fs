0fafefcdb76ee131e655e1365b8d4531434cd63c
kochser <s225169@student.dtu.dk>
2024-03-12 15:14:13 +0100

    Refactored compiler

--- code/src/Compiler.fs
+++ code/src/Compiler.fs
@@ -5,61 +5,7 @@ open Io.GCL
 open FSharp.Text.Lexing
 open System
 open AST
-
-exception ParseError of Position * string * Exception
-
-let rec printExpr expr =
-    match expr with
-    | Num(x) -> string x
-    | Var(x) -> x
-    | ListElement(x, y) -> sprintf "%s[%s]" x (printExpr y)
-    | TimesExpr(x, y) -> sprintf "(%s * %s)" (printExpr x) (printExpr y)
-    | DivExpr(x, y) -> sprintf "(%s / %s)" (printExpr x) (printExpr y)
-    | PlusExpr(x, y) -> sprintf "(%s + %s)" (printExpr x) (printExpr y)
-    | MinusExpr(x, y) -> sprintf "(%s - %s)" (printExpr x) (printExpr y)
-    | PowExpr(x, y) -> sprintf "(%s ^ %s)" (printExpr x) (printExpr y)
-    | UMinusExpr(x) -> sprintf "-%s" (printExpr x)
-
-let rec prettyPrintBool ast : string =
-    match ast with
-    | True -> "true"
-    | False -> "false"
-    | Not(b) -> sprintf "!%s" (prettyPrintBool b)
-    | And(b, b1) -> sprintf "(%s & %s)" (prettyPrintBool b) (prettyPrintBool b1)
-    | Or(b, b1) -> sprintf "(%s | %s)" (prettyPrintBool b) (prettyPrintBool b1)
-    | ShortAnd(b, b1) -> sprintf "(%s && %s)" (prettyPrintBool b) (prettyPrintBool b1)
-    | ShortOr(b, b1) -> sprintf "(%s || %s)" (prettyPrintBool b) (prettyPrintBool b1)
-    | Equal(b, b1) -> sprintf "(%s = %s)" (printExpr b) (printExpr b1)
-    | NotEqual(b, b1) -> sprintf "(%s != %s)" (printExpr b) (printExpr b1)
-    | Less(b, b1) -> sprintf "(%s < %s)" (printExpr b) (printExpr b1)
-    | LessEqual(b, b1) -> sprintf "(%s <= %s)" (printExpr b) (printExpr b1)
-    | Greater(b, b1) -> sprintf "(%s > %s)" (printExpr b) (printExpr b1)
-    | GreaterEqual(b, b1) -> sprintf "(%s >= %s)" (printExpr b) (printExpr b1)
-
-let createId x = x |> string |> (+) "q"
-
-let parse parser src =
-    let lexbuf = LexBuffer<char>.FromString src
-
-    let parser = parser Lexer.tokenize
-
-    try
-        Ok(parser lexbuf)
-    with e ->
-        let pos = lexbuf.EndPos
-        let line = pos.Line
-        let column = pos.Column
-        let message = e.Message
-        let lastToken = new String(lexbuf.Lexeme)
-        eprintf "Parse failed at line %d, column %d:\n" line column
-        eprintf "Last token: %s" lastToken
-        eprintf "\n"
-        Error(ParseError(pos, lastToken, e))
-
-let rec Done gc =
-    match gc with
-    | Implies(b, c) -> Not(b)
-    | GuardedOr(gc, gc1) -> And(Done gc, Done gc1)
+open Parser
 
 type Label =
     | CommandSkipLabel
@@ -72,75 +18,104 @@ type Edge =
       label: Label
       target: string }
 
-let rec countNodes command =
+let createNodeIdFromNodeCount x = x |> string |> (+) "q"
+
+let rec Done gc =
+    match gc with
+    | Implies(b, c) -> Not(b)
+    | GuardedOr(gc, gc1) -> And(Done gc, Done gc1)
+
+let rec NodesInCommand command =
     match command with
     | Skip -> 1
     | Assignment(var, expr) -> 1
     | ListAssignment(var, expr1, expr2) -> 1
-    | Program(c, c') -> countNodes c + countNodes c'
-    | If(gc) -> countGuadedNodes gc
-    | Do(gc) -> countGuadedNodes gc
+    | Program(c, c') -> NodesInCommand c + NodesInCommand c'
+    | If(gc) -> NodesInGuardedCommand gc
+    | Do(gc) -> NodesInGuardedCommand gc
 
-and countGuadedNodes gcommand =
+and NodesInGuardedCommand gcommand =
     match gcommand with
-    | Implies(b, c) -> 1 + countNodes c
-    | GuardedOr(gc, gc1) -> countGuadedNodes gc + countGuadedNodes gc1
+    | Implies(b, c) -> 1 + NodesInCommand c
+    | GuardedOr(gc, gc1) -> NodesInGuardedCommand gc + NodesInGuardedCommand gc1
 
-let rec edges command q1 q2 count determinism =
+let rec edges command q1 q2 nodesCount determinism =
     match command with
     | Skip ->
         [ { source = q1
             label = CommandSkipLabel
             target = q2 } ],
-        count
+        nodesCount
     | Assignment(var, expr) ->
         [ { source = q1
             label = CommandAssignmentLabel(var, expr)
             target = q2 } ],
-        count
+        nodesCount
     | ListAssignment(var, expr1, expr2) ->
         [ { source = q1
             label = CommandListAssignmentLabel(var, expr1, expr2)
             target = q2 } ],
-        count
+        nodesCount
     | Program(c, c') ->
-        let count' = countNodes c + count
-        let edge, _ = edges c q1 (createId count') count determinism
-        let edge', count'' = edges c' (createId count') q2 count' determinism
-        edge @ edge', count''
-    | If(gc) ->
-        let edge, count', _ = guardedEdges gc q1 q2 count determinism False
-        edge, count'
+        // The the id of the end node of the left program must be computed before edges are computed
+        let nodesInLeftProgram = NodesInCommand c + nodesCount
+
+        let edge, _ =
+            edges c q1 (createNodeIdFromNodeCount nodesInLeftProgram) nodesCount determinism
+
+        let edge', totalNodesCount =
+            edges c' (createNodeIdFromNodeCount nodesInLeftProgram) q2 nodesInLeftProgram determinism
+
+        edge @ edge', totalNodesCount
+    | If(gc) -> guardedEdges gc q1 q2 nodesCount determinism
     | Do(gc) ->
-        let edge, count', _ = guardedEdges gc q1 q1 count determinism False
+        let edge, nodesCount = guardedEdges gc q1 q1 nodesCount determinism
 
-        { source = q1
-          label = BoolLabel(Done(gc))
-          target = q2 }
-        :: edge,
-        count' + 1
+        let doneEdge =
+            { source = q1
+              label = BoolLabel(Done(gc))
+              target = q2 }
 
-// Returns (edge, count, nextBool)
-and guardedEdges gcommand q1 q2 count determinism previousBool =
+        doneEdge :: edge, nodesCount + 1
+
+and guardedEdges gcommand q1 q2 nodesCount determinism =
+    let edge, nodesCount, _ =
+        guardedEdgesHelper gcommand q1 q2 nodesCount determinism False
+
+    edge, nodesCount
+
+and guardedEdgesHelper gcommand q1 q2 nodesCount determinism previousBool : Edge list * int * bool =
     match gcommand with
     | Implies(b, c) ->
-        let edge, count' = edges c (createId (count + 1)) q2 (count + 1) determinism
+        // An intermediate node must be created
+        // A boolean edge goes from source node into intermediate node
+        // and the program uses the intermediate node as the source node
+
+        let intermediateNodeNumber = nodesCount + 1
+        let intermediateNodeId = createNodeIdFromNodeCount intermediateNodeNumber
 
         let bool, nextBool =
             match determinism with
             | NonDeterministic -> b, False
             | Deterministic -> And(b, Not(previousBool)), Or(b, previousBool)
 
-        { source = q1
-          label = BoolLabel(bool)
-          target = createId (count + 1) }
-        :: edge,
-        count',
-        nextBool
+        let intermediateNode =
+            { source = q1
+              label = BoolLabel(bool)
+              target = intermediateNodeId }
+
+        let edge, totalNodes =
+            edges c intermediateNodeId q2 intermediateNodeNumber determinism
+
+        intermediateNode :: edge, totalNodes, nextBool
     | GuardedOr(gc1, gc2) ->
-        let edge, count', nextBool = guardedEdges gc1 q1 q2 count determinism previousBool
-        let edge', count'', nextBool' = guardedEdges gc2 q1 q2 count' determinism nextBool
-        edge @ edge', count'', nextBool'
+        let edge, leftGCNodesCount, nextBool =
+            guardedEdgesHelper gc1 q1 q2 nodesCount determinism previousBool
+
+        let edge', toalNodesCount, nextBool' =
+            guardedEdgesHelper gc2 q1 q2 leftGCNodesCount determinism nextBool
+
+        edge @ edge', toalNodesCount, nextBool'
 
 let rec printLabel label =
     match label with
