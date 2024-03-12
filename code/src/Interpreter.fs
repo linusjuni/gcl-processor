43068c89a6a09be31b1f6675e1b573301a3c8beb
kochser <s225169@student.dtu.dk>
2024-03-12 14:33:41 +0100

    Added memory get and update

--- code/src/Interpreter.fs
+++ code/src/Interpreter.fs
@@ -1,5 +1,6 @@
 module Interpreter
 open Io.Interpreter
+open Io.GCL
 
 let analysis (input: Input) : Output =
     failwith "Interpreter not yet implemented" // TODO: start here
@@ -9,3 +10,17 @@ let analysis (input: Input) : Output =
       dot = ""
       trace = []
       termination = Terminated }
+
+let getValueOfVariable variable (memory: InterpreterMemory) : int64 =
+    Map.find variable memory.variables
+
+let getValueOfArray array (memory: InterpreterMemory) : List<int64> =
+    Map.find array memory.arrays
+
+let updateVariable variable value (memory: InterpreterMemory) : InterpreterMemory =
+    { variables = Map.add variable value memory.variables
+      arrays = memory.arrays }
+
+let updateArray array value (memory: InterpreterMemory) : InterpreterMemory =
+    { variables = memory.variables
+      arrays = Map.add array value memory.arrays }
